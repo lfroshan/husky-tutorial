5c13397559425fe7df0bc1afb64bc89ba9bb7c39
Roshan Shrestha <roshanshrestha1@lftechnology.com>
2022-03-28 14:46:22 +0545

    sdfa

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 class Car {
-  constructor(brand) {
-    this.brand = brand;
+  constructor (brand) {
+    this.brand = brand
   }
 }
 
 class Model extends Car {
-  constructor(brand, model) {
-    super(brand);
-    this.model = model;
+  constructor (brand, model) {
+    super(brand)
+    this.model = model
   }
 
-  get modelName() {
-    return this.model;
+  get modelName () {
+    return this.model
   }
 
-  get brandName() {
-    return this.brand;
+  get brandName () {
+    return this.brand
   }
 
-  set modelName(model) {
-    this.model = model;
+  set modelName (model) {
+    this.model = model
   }
 
-  set brandName(brand) {
-    this.brand = brand;
+  set brandName (brand) {
+    this.brand = brand
   }
 }
 
-const kia = new Model("Hyundai", "i20");
-console.log(kia);
+const kia = new Model('Hyundai', 'i20')
+console.log(kia)
 console.log(
-  "This line is not properly written in local but this will be properly maintained in git because of husky."
-);
-console.log();
+  'This line is not properly written in local but this will be properly maintained in git because of husky.'
+)
+console.log()
 
-         console.log("again another line.");
+console.log('again another line.')
 
-console.log("removed prettier.")
+console.log('removed prettier.')
+console.log('safsdafds')
 
-    console.log("I don't know what is happening.")
+console.log("I don't know what is happening.")
